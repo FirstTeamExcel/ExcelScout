475f1fc0fb6bae6121d6c12a0a99086d5bd6db65
DjScribbles <joeshaw321@gmail.com>
2013-10-07 18:26:43 -0400

    Initial Commit
    Some basic xaml code in place.

---
+++ View/RobotInfoView.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ExcelScout.View
+{
+    /// <summary>
+    /// Interaction logic for RobotInfoView.xaml
+    /// </summary>
+    public partial class RobotInfoView : UserControl
+    {
+        public RobotInfoView()
+        {
+            InitializeComponent();
+        }
+    }
+}
