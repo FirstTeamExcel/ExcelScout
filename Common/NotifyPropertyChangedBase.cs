836c7f2958b9c5cfc0753153bb5803a1a5f3929c
DjScribbles <joeshaw321@gmail.com>
2013-10-08 08:33:11 -0400

    Added a data binding example

---
+++ Common/NotifyPropertyChangedBase.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace ExcelScout.Common
+{
+    class NotifyPropertyChangedBase : INotifyPropertyChanged
+    {
+
+
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void NotifyPropertyChanged(string propertyName)
+        {
+            PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+
+
+    }
+}
---
+++ ViewModel/ExampleViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ExcelScout.Common;
+
+namespace ExcelScout.ViewModel 
+{
+    class ExampleViewModel : NotifyPropertyChangedBase
+    {
+        private string _FavoriteColor;
+        public string FavoriteColor 
+        {
+            get
+            {
+                return _FavoriteColor;
+            }
+            set
+            {
+                _FavoriteColor = value;
+                NotifyPropertyChanged("FavoriteColor");
+            }
+        }
+    }
+}
