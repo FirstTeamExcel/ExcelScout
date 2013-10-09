a2486ca1b652337aca5665fb46ed43efba58a054
DjScribbles <joeshaw321@gmail.com>
2013-10-08 20:19:33 -0400

    Added basic team view
    Ton of stuff

---
+++ ViewModel/TeamViewModel.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ExcelScout.Common;
+namespace ExcelScout.ViewModel
+{
+    class TeamViewModel:NotifyPropertyChangedBase
+    {
+        private string _Name;
+        public string Name
+        {
+            get
+            {
+                return _Name;
+            }
+            set
+            {
+                _Name = value;
+                NotifyPropertyChanged("Name");
+                NotifyPropertyChanged("TeamTitle");
+            }
+        }
+        private int _Number;
+        public int Number
+        {
+            get
+            {
+                return _Number;
+            }
+            set
+            {
+                _Number = value;
+                NotifyPropertyChanged("Number");
+                NotifyPropertyChanged("TeamTitle");
+            }
+        }
+
+        public string TeamTitle
+        {
+            get
+            {
+                return string.Format("Team {1} Number {0}", Number, Name);
+            }
+        }
+    }
+}
